ac608c45c220a73a2dc94eb37598ab009ef7875c
namubar <mubarakshin.nail@tatar.ru>
2022-11-11 22:14:07 +0300

    initial

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿int[] array = new int[5];                  //Создание массива и заполнение случайными трехзначными числами;
+int i;
+void fillArray(int[] array)
+{
+    Random rnd = new Random();
+
+    for (i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(100, 1000);
+        Console.Write(array[i] + " ");
+    }
+}
+fillArray(array);
+Console.WriteLine();
+
+int sumDigit(int[] array)
+{
+    int count = 0;                               //Количество четных чисел;
+    for (i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+int count = sumDigit(array);
+Console.WriteLine(" Количество четных чисел в массиве: " + count);
